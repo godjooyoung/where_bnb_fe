2018339fe7599fee225964cf8393e8d50255f8c8
godjooyoung <xolovesjoo36@naver.com>
2023-05-17 03:32:33 +0900

    feat 등록  폼데이터 세팅

--- src/api/room.js
+++ src/api/room.js
@@ -1,6 +1,7 @@
 import { multipartInstance }from '../api/apiConfig'
 
 const roomRegister = async (data) => {
+    console.log("data!!!!!", data)
     const response = await multipartInstance.post('/room', data)
     console.log("____",response)
     return response.data
--- src/components/roomRegiser/registerStep4/RegiEnd.jsx
+++ src/components/roomRegiser/registerStep4/RegiEnd.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { styled } from 'styled-components';
 import RoomRegiTitle from '../registerStepComponent/RoomRegiTitle';
+import { getCookie } from "../../../cookie/Cookie";
 
 function RegiEnd(props) {
-
+    const welcomeTitle = getCookie('userName')+' 님, 축하합니다!'
+    
     return (
         <StepDiv>
             <StepWrapDiv>
-                <RoomRegiTitle title="{사용자명} 님, 축하합니다!" alert="웨어비앤비 호스트가 되신 것을 진심으로 환영합니다. 숙소 호스팅을 통해 게⁠스⁠트⁠에⁠게 놀⁠라⁠운 경⁠험⁠을 선⁠사⁠하⁠는 데 동⁠참⁠해⁠주⁠셔⁠서 감⁠사⁠합⁠니⁠다." />
+                <RoomRegiTitle title={welcomeTitle} alert="웨어비앤비 호스트가 되신 것을 진심으로 환영합니다. 숙소 호스팅을 통해 게⁠스⁠트⁠에⁠게 놀⁠라⁠운 경⁠험⁠을 선⁠사⁠하⁠는 데 동⁠참⁠해⁠주⁠셔⁠서 감⁠사⁠합⁠니⁠다." />
                 <div style={{ width: '100%', height: '15px' }}></div>
                 
             </StepWrapDiv>
